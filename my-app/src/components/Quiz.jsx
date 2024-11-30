d0878e6b3100809a9be2abfffa92f1586211a4dd
bbkbyti <bbkbyti@gmail.com>
2024-11-30 14:35:51 -0600

    answers shuffle using sort method. when user picks max number of answers app stops and shows a message to the user.

--- my-app/src/components/Quiz.jsx
+++ my-app/src/components/Quiz.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 
 import QUESTIONS from '../questions.js'
 
-
+//task: Shuffle the answers, and add a limit to the number of answers that they they app does not gives error
 export default function Quiz() {
     const [userAns, setUserAns] = useState([]);
 
     // instead of using more state, more state could be derived from existing ones.
     const activeQuestionIndex = userAns.length;
+
+    const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAns((prevState) => {
@@ -15,13 +17,21 @@ export default function Quiz() {
         });
     }
 
-    //task: switching between questions and get the answer from the user
+    if (quizComplete) {
+        return (
+            <div id='summary'>
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
     return (
         <div id='quiz'>
             <div id='question'>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
