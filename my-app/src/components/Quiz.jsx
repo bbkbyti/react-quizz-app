9e5a373f90bcb3c6df52acc5ce645d977fb64c68
bbkbyti <bbkbyti@gmail.com>
2024-11-30 19:24:48 -0600

    color of the answers change when hover or clicked but they jump around. trying to fix this

--- my-app/src/components/Quiz.jsx
+++ my-app/src/components/Quiz.jsx
@@ -3,20 +3,37 @@ import React, { useState, useCallback } from 'react'
 import QUESTIONS from '../questions.js'
 import QuestionTimer from './QuestionTimer.jsx';
 
-//task: Shuffle the answers, and add a limit to the number of answers that they they app does not gives error
+//task: As the user picks an answer first highlight that answer and then after a second the color of the answer
+// changes to green if it was the correct answer and red if it wasn't
+
 export default function Quiz() {
     const [userAns, setUserAns] = useState([]);
+    const [answerState, setAnswerState] = useState('')
 
     // instead of using more state, more state could be derived from existing ones.
-    const activeQuestionIndex = userAns.length;
+    const activeQuestionIndex = answerState === '' ? userAns.length : userAns.length - 1;
 
     const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
-    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(
+        selectedAnswer
+    ) {
+        setAnswerState('answered');
         setUserAns((prevState) => {
             return [...prevState, selectedAnswer];
         });
-    }, [])
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                // first option of the questions is the correct answer in questions.js questions.
+                setAnswerState('correct')
+            } else {
+                setAnswerState('wrong');
+            }
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000);
+        }, 1000);
+    }, [activeQuestionIndex]);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -32,6 +49,9 @@ export default function Quiz() {
     shuffledAnswers.sort(() => Math.random() - 0.5);
 
 
+
+
+
     return (
         <div id='quiz'>
             <div id='question'>
@@ -42,11 +62,27 @@ export default function Quiz() {
                 />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    {shuffledAnswers.map((answer) => {
+
+                        const isSelected = userAns[userAns.length - 1] === answer;
+                        let cssClass = '';
+
+                        if (answerState === 'answered' && isSelected) {
+                            cssClass = 'selected';
+                        }
+
+                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                            cssClass = answerState;
+                        }
+                        return <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}
+                                className={cssClass}
+                            >
+                                {answer}</button>
                         </li>
-                    ))}
+                    }
+
+                    )}
                 </ul>
             </div>
         </div>
