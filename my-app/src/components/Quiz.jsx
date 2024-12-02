abda8d94454743c743faaf5e9a6fbafb80d9e634
bbkbyti <bbkbyti@gmail.com>
2024-12-02 14:37:06 -0600

    managing the state from Questin.jsx component itself where it is being used and moved the uneccassary states from the Quiz.jsx

--- my-app/src/components/Quiz.jsx
+++ my-app/src/components/Quiz.jsx
@@ -3,38 +3,25 @@ import React, { useState, useCallback } from 'react'
 import QUESTIONS from '../questions.js'
 import Question from './Question.jsx'
 
-//task: As the user picks an answer first highlight that answer and then after a second the color of the answer
-// changes to green if it was the correct answer and red if it wasn't
+//task:
 
 export default function Quiz() {
 
     const [userAns, setUserAns] = useState([]);
-    const [answerState, setAnswerState] = useState('')
+
 
     // instead of using more state, more state could be derived from existing ones.
-    const activeQuestionIndex = answerState === '' ? userAns.length : userAns.length - 1;
+    const activeQuestionIndex = userAns.length;
 
     const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(
         selectedAnswer
     ) {
-        setAnswerState('answered');
         setUserAns((prevState) => {
             return [...prevState, selectedAnswer];
         });
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                // first option of the questions is the correct answer in questions.js questions.
-                setAnswerState('correct')
-            } else {
-                setAnswerState('wrong');
-            }
-            setTimeout(() => {
-                setAnswerState('');
-            }, 2000);
-        }, 1000);
-    }, [activeQuestionIndex]);
+    }, []);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -51,11 +38,9 @@ export default function Quiz() {
         <div id='quiz'>
             <div id='question'>
                 <Question
-                    questionText={QUESTIONS[activeQuestionIndex].text}
-                    answers={QUESTIONS[activeQuestionIndex].answers}
+                    key={activeQuestionIndex}
+                    index={activeQuestionIndex}
                     onSelectAnswer={handleSelectAnswer}
-                    answerState={answerState}
-                    selectedAnswer={userAns[userAns.length - 1]}
                     onSkipAnswer={handleSkipAnswer}
                 />
             </div>
